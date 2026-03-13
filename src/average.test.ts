2b309f5114af785627de6550ff1e7ed51f13b184
Sarah <sarahchristen74@gmail.com>
2026-03-13 15:15:14 +0100

    aufgabe 5.2: testfall durchschnitt von 5 zahlen

--- src/average.test.ts
+++ src/average.test.ts
@@ -11,3 +11,15 @@ test("test mean of two numbers", () => {
   // Assert
   expect(actual).toBe(expected);
 });
+
+test("test mean of five numbers", () => {
+  // Arrange
+  const numbers: number[] = [1, 9, 2, 7, 2];
+  const expected: number = 4.2;
+
+  // Act
+  const actual: number = mean(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
