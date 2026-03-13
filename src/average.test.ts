78ae590f513cd4b4f0a28a32e0dc0181b12b1deb
Sarah <sarahchristen74@gmail.com>
2026-03-13 15:12:56 +0100

    aufgabe 5.1: testfall angepasst

--- src/average.test.ts
+++ src/average.test.ts
@@ -2,8 +2,8 @@ import { mean } from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
-  const numbers: number[] = [1, 4];
-  const expected: number = 2.5;
+  const numbers: number[] = [1, 9];
+  const expected: number = 5;
 
   // Act
   const actual: number = mean(numbers);
